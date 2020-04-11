054c83c60376b98aac192f47e31cd3ee4aef87bb
WalterWillis <waltertwillis@outlook.com>
2020-04-10 21:00:41 -0400

    Footer Cleanup, UART UI Addition
    Started working on the UART footer logic. I'm thinking all inject the vibe client inside of the inputmodel for the serial port, but i'm not sure yet.
    
    Cleaned up the footer since I had the chance to do it. Now it is the same across all pages. since it is part of the main layout, it's the same even when navigating to different pages.

---
+++ BlazorWebSpaceTool/BlazorWebSpaceTool/InputModels/SerialPortInputModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO.Ports;
+
+namespace BlazorWebSpaceTool.InputModels
+{
+    public class SerialPortInputModel
+    {
+        public string[] AvailablePorts { get => SerialPort.GetPortNames() ?? new string[] { "None" }; }
+
+        [Required(ErrorMessage = "Select a port", AllowEmptyStrings = false)]
+        public string SelectedPort { get; set; }
+
+        [Required(ErrorMessage = "There needs to be a message to send", AllowEmptyStrings = false)]
+        public string Text { get; set; }
+
+        public void SendText()
+        {
+            if (!string.IsNullOrEmpty(SelectedPort) && !string.IsNullOrEmpty(Text))
+            {
+                try
+                {
+                    using (SerialPort serialPort = new SerialPort(SelectedPort))
+                    {
+                        serialPort.Open();
+                        serialPort.Write(Text);
+                        serialPort.Close();
+                    }
+                }
+                catch(Exception ex)
+                {
+                    ErrorMessage = ex.Message;
+                }
+            }
+        }
+
+        public string ErrorMessage { get; set; }
+    }
+}
