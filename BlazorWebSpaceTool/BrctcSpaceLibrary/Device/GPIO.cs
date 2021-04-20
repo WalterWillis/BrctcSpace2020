c8376ae984991c0ede1c0bd94d17e2fee567d62b
WalterWillis <waltertwillis@outlook.com>
2021-04-20 19:12:53 -0400

    New RPI Rev 1.2 .net GPIO changes
    Bought a new RPI 4 GB model. This board was a Rev 1.2, which caused the PinNumberingScheme.Board schema to stop working. Had to modify it for it to work.
    
    Added the troubleshooting logs and test results following the fix.

--- BlazorWebSpaceTool/BrctcSpaceLibrary/Device/GPIO.cs
+++ BlazorWebSpaceTool/BrctcSpaceLibrary/Device/GPIO.cs
@@ -8,12 +8,12 @@ namespace BrctcSpaceLibrary.Device
     public class GPIO : IGPIO
     {
         private GpioController _gpio;
-        private const int DR_PIN = 13; //physical pin scheme;
-        private const int RST_PIN = 15;
+        private const int DR_PIN = 27; //physical pin scheme;
+        private const int RST_PIN = 22;
 
         public GPIO()
         {
-            _gpio = new GpioController(PinNumberingScheme.Board);
+            _gpio = new GpioController(PinNumberingScheme.Logical);
             _gpio.OpenPin(DR_PIN, PinMode.Input);
             _gpio.OpenPin(RST_PIN, PinMode.Output);
             _gpio.Write(RST_PIN, PinValue.High);  //RST pin should always be High unless we want to reset the gyro
@@ -29,4 +29,11 @@ namespace BrctcSpaceLibrary.Device
             _gpio.UnregisterCallbackForPinValueChangedEvent(DR_PIN, callback);
         }
     }
+
+    /*
+     If the error below is recieved, the issue is due to the compatibility of the PinNumberingScheme and the current revision of the pi.
+    For example: PinNumberingScheme.Board works on Rev 1.1, but not Rev 1.2. We had to move to PinNumberingScheme.Logical on a Rev 1.2 Pi4
+    
+     Unhandled exception. System.PlatformNotSupportedException: This driver is generic so it can not perform conversions between pin numbering schemes.
+    */
 }
